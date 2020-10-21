d50081136cccbc207ec9dfe35b9739e12dca64ba
dagute <dagute28@gmail.com>
2020-10-21 03:20:38 +0000

    adding tasks 3 and 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Tasks"""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    returns the sorted list of delays.
+    """
+    x = await asyncio.gather(*(task_wait_random(max_delay) for i in range(n)))
+    return sorted(x)
