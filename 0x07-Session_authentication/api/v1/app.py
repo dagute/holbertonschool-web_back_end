a7916b436a064090fc8d80e97875e267f80be63d
dagute <dagute28@gmail.com>
2020-11-11 16:14:38 +0000

    add session_db

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -11,9 +11,7 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
-"""
 from api.v1.auth.session_db_auth import SessionDBAuth
-"""
 
 
 app = Flask(__name__)
