6b005ace7e1ad8c4e0428baa769316cbaeebf8cb
dagute <dagute28@gmail.com>
2020-11-18 04:06:55 +0000

    first commit

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -7,7 +7,6 @@ from sqlalchemy.orm import sessionmaker
 from typing import TypeVar, Any
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-
 from user import Base, User
 
 
@@ -15,7 +14,7 @@ class DB:
     """DB class"""
     def __init__(self):
         """Constructor"""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -35,24 +34,28 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **filters: Any) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """ returns the first row found in the
         users table as filtered by the method’s
         input arguments"""
-        if not User.__dict__.get(*filters):
+        if not kwargs:
             raise InvalidRequestError
-        query = self._session.query(User).filter_by(**filters)
-        if not query.first():
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError
+        query = self._session.query(User).filter_by(**kwargs).first()
+        if query is None:
             raise NoResultFound
-        return query.first()
+        return query
 
-    def update_user(self, user_id: int, **kwargs: Any):
+    def update_user(self, user_id: int, **kwargs) -> None:
         """Uses find_user_by to locate the user"""
         try:
             user = self.find_user_by(id=user_id)
+            column_names = User.__table__.columns.keys()
             for key, val in kwargs.items():
                 setattr(user, key, val)
             self._session.commit()
-            return None
         except Exception:
             raise ValueError
