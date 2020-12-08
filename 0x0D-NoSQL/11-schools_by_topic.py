dd85ab9ec3e8ffda568e31c1c418dbdd0a43b4c8
dagute <dagute28@gmail.com>
2020-12-08 16:03:40 +0000

    first commit

--- 0x0D-NoSQL/11-schools_by_topic.py
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -4,4 +4,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """Returns the list of school having specific topic"""
-    return (mongo_collection.find({"topics": topic}))
+    return mongo_collection.find({"topics": topic})
