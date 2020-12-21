23cfc49fae43d7ba29d3fec481623803e9c7aabb
dagute <dagute28@gmail.com>
2020-12-21 17:06:28 +0000

    fixing white spaces

--- 0x10-ES6_classes/8-hbtn_class.js
+++ 0x10-ES6_classes/8-hbtn_class.js
@@ -3,7 +3,7 @@ export default class HolbertonClass {
     this._size = size;
     this._location = location;
   }
-  
+
   [Symbol.toPrimitive](type) {
     if (type === 'string') return this._location;
     if (type === 'number') return this._size;
