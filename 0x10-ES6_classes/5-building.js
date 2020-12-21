9f3dfedfae5252b2e245c935f928da08f390e1c2
dagute <dagute28@gmail.com>
2020-12-21 17:00:59 +0000

    A Building

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -2,14 +2,14 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
     if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      throw Error('Class extending Building must override evacuationWarningMessage',);
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
   set sqft(value) {
     this._sqft = value;
   }
-  
+
   get sqft() {
     return this._sqft;
   }
