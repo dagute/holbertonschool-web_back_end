12104d2ebf2741fdf2eb991c97b3eed7e3a49591
dagute <dagute28@gmail.com>
2020-12-21 16:53:08 +0000

    Pricing

--- 0x10-ES6_classes/4-pricing.js
+++ 0x10-ES6_classes/4-pricing.js
@@ -5,27 +5,27 @@ export default class Pricing {
     this._amount = amount;
     this._currency = currency;
   }
-  
-  get amount() {
-    return this._amount;
-  }
-  
-  get currency() {
-    return this._currency;
-  }
-  
+
   set amount(newAmount) {
     this._amount = newAmount;
   }
-  
+
+  get amount() {
+    return this._amount;
+  }
+
   set currency(newCurrency) {
     this._currency = newCurrency;
   }
-  
-  displayFullPrice() {
-    return `${this.amount} (${this.currency.displayFullCurrency()})`;
+
+  get currency() {
+    return this._currency;
   }
-  
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.displayFullCurrency()}`;
+  }
+
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
