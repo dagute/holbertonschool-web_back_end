da2575c6bddac6dc54263375014d10ae0fbed4f8
dagute <dagute28@gmail.com>
2021-01-09 22:06:00 +0000

    Using Process stdin

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,11 +1,12 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.resume();
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  if (name) console.log(`Your name is: ${name}`);
-});
-
-if (process.stdout.isTTY) {
-  process.stdin.on('end', () => {
+  process.stdout.write(`Your name is: ${name}`);
+  if (process.stdin.isTTY) {
+    process.exit();
+  } else {
     process.stdout.write('This important software is now closing\n');
-  });
-}
+    process.exit();
+  }
+});
