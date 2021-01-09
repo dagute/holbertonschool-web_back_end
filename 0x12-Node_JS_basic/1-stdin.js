ef5b7b3fbe59815100c8c3c66c74a894a0ac65c8
dagute <dagute28@gmail.com>
2021-01-09 22:00:01 +0000

    first commit

---
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) console.log(`Your name is: ${name}`);
+});
+
+if (process.stdout.isTTY) {
+  process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
