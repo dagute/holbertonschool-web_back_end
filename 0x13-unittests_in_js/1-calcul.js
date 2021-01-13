347f96b41cf77c37edade41c5721f38910dce713
dagute <dagute28@gmail.com>
2021-01-13 21:02:26 +0000

    Combining descriptions

--- 0x13-unittests_in_js/1-calcul.js
+++ 0x13-unittests_in_js/1-calcul.js
@@ -1,20 +1,20 @@
 function calculateNumber(type, x, y) {
-    const xRound = Math.round(x);
-    const yRound = Math.round(y);
+  const xRound = Math.round(x);
+  const yRound = Math.round(y);
   
-    if (type === 'DIVIDE') {
-      if (xRound === 0) {
-        return 'Error';
-      }
-      return xRound / yRound;
+  if (type === 'DIVIDE') {
+    if (xRound === 0) {
+      return 'Error';
     }
-  
-    if (type === 'SUBTRACT') {
-      return xRound - yRound;
-    }
-    
-    return xRound + yRound;
+    return xRound / yRound;
   }
   
-  module.exports = calculateNumber;
+  if (type === 'SUBTRACT') {
+    return xRound - yRound;
+  }
+    
+  return xRound + yRound;
+}
+  
+module.exports = calculateNumber;
   
