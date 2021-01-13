759b203aefd5d5056c6e922fe9511dff415f0955
dagute <dagute28@gmail.com>
2021-01-13 21:01:19 +0000

    Combining descriptions

---
+++ 0x13-unittests_in_js/1-calcul.js
@@ -0,0 +1,20 @@
+function calculateNumber(type, x, y) {
+    const xRound = Math.round(x);
+    const yRound = Math.round(y);
+  
+    if (type === 'DIVIDE') {
+      if (xRound === 0) {
+        return 'Error';
+      }
+      return xRound / yRound;
+    }
+  
+    if (type === 'SUBTRACT') {
+      return xRound - yRound;
+    }
+    
+    return xRound + yRound;
+  }
+  
+  module.exports = calculateNumber;
+  
