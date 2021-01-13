2fc38c545c3767afacaad99b46ce4a34fda11439
dagute <dagute28@gmail.com>
2021-01-13 21:18:39 +0000

    Combining descriptions

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -4,11 +4,11 @@ const assert = require("assert");
 
 describe('calculateNumber', () => {
   it('returns rounded sum with SUM', () => {
-    assert.strictEqual(calculateNumber(2, 2), 4);
-    assert.strictEqual(calculateNumber(1.5, 3.2), 5);
-    assert.strictEqual(calculateNumber(1.3, 3.7), 5);
-    assert.strictEqual(calculateNumber(-1, -3), -4);
-    assert.strictEqual(calculateNumber(-1.2, -3.8), -5);
+  assert.strictEqual(calculateNumber(2, 2), 4);
+  assert.strictEqual(calculateNumber(1.5, 3.2), 5);
+  assert.strictEqual(calculateNumber(1.3, 3.7), 5);
+  assert.strictEqual(calculateNumber(-1, -3), -4);
+  assert.strictEqual(calculateNumber(-1.2, -3.8), -5);
   });
   it('returns rounded sum with SUBTRACT', () => {
       assert.strictEqual(calculateNumber('SUBTRACT', 2, 4), -2);
@@ -16,7 +16,7 @@ describe('calculateNumber', () => {
       assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4), -3);
       assert.strictEqual(calculateNumber('SUBTRACT', -1, -3), 2);
       assert.strictEqual(calculateNumber('SUBTRACT',-1.4, -3.6), 3);
-    });
+  });
   it('returns rounded sum with DIVIDE', () => {
       assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
   });
