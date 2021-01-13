951e7dde62fb5b640bb21117c38c3c95da8c5544
dagute <dagute28@gmail.com>
2021-01-13 21:12:02 +0000

    Combining descriptions

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -11,16 +11,16 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(-1.2, -3.8), -5);
   });
   it('returns rounded sum with SUBTRACT', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 2, 4), -2);
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.7, 3), -1);
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 3.6), -3);
-    assert.strictEqual(calculateNumber('SUBTRACT', -1, -3), 2);
-    assert.strictEqual(calculateNumber('SUBTRACT',-1.5, -3.5), 3);
+      assert.strictEqual(calculateNumber('SUBTRACT', 2, 4), -2);
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.7, 3), -1);
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4), -3);
+      assert.strictEqual(calculateNumber('SUBTRACT', -1, -3), 2);
+      assert.strictEqual(calculateNumber('SUBTRACT',-1.4, -3.6), 3);
     });
-    it('returns rounded sum with DIVIDE', () => {
-        assert.strictEqual(calculateNumber('DIVIDE', 1.7, 4.2), 0.4);
-    });
-    it('returns error string when DIVIDE by 0', () => {
-        assert.strictEqual(calculateNumber('DIVIDE', 1.7, 0), 'Error');
-    });
+  it('returns rounded sum with DIVIDE', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  });
+  it('returns error string when DIVIDE by 0', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.7, 0), 'Error');
+  });
 });
